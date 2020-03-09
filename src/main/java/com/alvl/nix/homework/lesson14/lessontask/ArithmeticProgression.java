281e160a8458bbd1b22ea83942be983b25cbee47
Yehor Bondar <egor.bondar.82@gmail.com>
2020-03-09 21:32:26 +0200

    Add lesson 14  task

---
+++ src/main/java/com/alvl/nix/homework/lesson14/lessontask/ArithmeticProgression.java
@@ -0,0 +1,39 @@
+package com.alvl.nix.homework.lesson14;
+
+
+public class ArithmeticProgression {
+    private final int initial;
+    private final int step;
+
+    ArithmeticProgression(int initial, int step) throws ProgressionConfigurationException {
+        this.initial = initial;
+        if (step == 0)
+            throw new ProgressionConfigurationException("Can't calculate progression!Enter step except 0!");
+        else this.step = step;
+    }
+
+    int calculate(int n) throws ProgressionConfigurationException {
+        int result = initial, count = 0;
+        if (n <= 0)
+            throw new ProgressionConfigurationException("You entered too small number, try n > 0!");
+        else {
+            while (count != n) {
+                result=result+step;
+                count++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws ProgressionConfigurationException {
+        try {
+            ArithmeticProgression arithmeticProgression = new ArithmeticProgression(0, 7);
+            arithmeticProgression.calculate(5);
+        }
+        catch (ProgressionConfigurationException prgexc)
+        {
+            prgexc.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/alvl/nix/homework/lesson14/lessontask/ProgressionConfigurationException.java
@@ -0,0 +1,19 @@
+package com.alvl.nix.homework.lesson14;
+
+public class ProgressionConfigurationException extends Exception {
+    public ProgressionConfigurationException() {
+        super();
+    }
+
+    public ProgressionConfigurationException(String message) {
+        super(message);
+    }
+
+    public ProgressionConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProgressionConfigurationException(Throwable cause) {
+        super(cause);
+    }
+}
